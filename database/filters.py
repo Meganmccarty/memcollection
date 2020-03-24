0abc850a618cb084b2c7ec9df7db954906b690af
Megan McCarty <megan_mccarty@hotmail.com>
2020-03-24 03:02:41 +0000

    Modified specimen record filter to allow typing terms in fields

--- database/filters.py
+++ database/filters.py
@@ -5,8 +5,37 @@ import django_filters
 
 class SpecimenRecordFilter(django_filters.FilterSet):
     usi = django_filters.CharFilter(lookup_expr='icontains', label='Specimen Identifier')
+    order = django_filters.CharFilter(field_name='order__order_name', lookup_expr='icontains',
+                                        label='Order')
+    family = django_filters.CharFilter(field_name='family__family_name',
+                                        lookup_expr='icontains', label='Family')
+    subfamily = django_filters.CharFilter(field_name='subfamily__subfamily_name',
+                                        lookup_expr='icontains', label='Subfamily')
+    tribe = django_filters.CharFilter(field_name='tribe__tribe_name', lookup_expr='icontains',
+                                        label='Tribe')
+    genus = django_filters.CharFilter(field_name='genus__genus_name', lookup_expr='icontains',
+                                        label='Genus')
+    species = django_filters.CharFilter(field_name='species__species_name',
+                                        lookup_expr='icontains', label='Species')
+    subspecies = django_filters.CharFilter(field_name='subspecies__subspecies_name',
+                                        lookup_expr='icontains', label='Subspecies')
+    common_name = django_filters.CharFilter(field_name='common_name__common_name',
+                                        lookup_expr='icontains', label='Common name')
+    mona = django_filters.CharFilter(field_name='mona__mona', lookup_expr='icontains',
+                                        label="MONA #")
+    p3 = django_filters.CharFilter(field_name='p3__p3', lookup_expr='icontains', label='P3 #')
     determined_year = django_filters.CharFilter(lookup_expr='icontains', label='Determined year')
     preparation_date = django_filters.DateFilter(lookup_expr='icontains', label='Preparation date')
+    county = django_filters.CharFilter(field_name='county__county_name', lookup_expr='icontains',
+                                        label='County')
+    locality = django_filters.CharFilter(field_name='locality__locality_name',
+                                        lookup_expr='icontains', label='Locality')
+    gps_latitude = django_filters.CharFilter(field_name='gps__latitude',
+                                        lookup_expr='icontains', label='Latitude')
+    gps_longitude = django_filters.CharFilter(field_name='gps__longitude',
+                                        lookup_expr='icontains', label='Longitude')
+    gps_elevation = django_filters.CharFilter(field_name='gps__elevation',
+                                        lookup_expr='icontains', label='Elevation')
     day = django_filters.CharFilter(lookup_expr='icontains', label='Day')
     month = django_filters.CharFilter(lookup_expr='icontains', label='Month')
     year = django_filters.CharFilter(lookup_expr='icontains', label='Year')
